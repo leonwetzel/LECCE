973551f7b3fe26a5396f85240ade6cd2cdf5baa5
Leon Wetzel <l.f.a.wetzel@student.rug.nl>
2020-10-05 13:50:19 +0200

    * baseline systeem verbeterd

--- baseline/main.py
+++ baseline/main.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 import csv
+import sys
 
 import pandas as pd
-from sklearn.metrics import make_scorer, f1_score, accuracy_score, \
-    precision_score, recall_score, precision_recall_fscore_support, \
-    confusion_matrix, mean_squared_error, r2_score, \
+from sklearn.metrics import mean_squared_error, r2_score, \
     explained_variance_score, max_error, mean_absolute_error
 
 from sklearn.model_selection import GridSearchCV
-from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 
 COLUMN_NAMES = ['id', 'subcorpus', 'sentence', 'token', 'complexity']
 MULTI_TRIAL_FILE_NAME = "lcp_multi_trial.tsv"
 SINGLE_TRIAL_FILE_NAME = "lcp_single_trial.tsv"
 MULTI_TRAIN_FILE_NAME = "lcp_multi_train.tsv"
 SINGLE_TRAIN_FILE_NAME = "lcp_single_train.tsv"
+
+ENCODER = LabelEncoder()
 
 pd.set_option('display.max_rows', None)
 
@@ -27,17 +27,26 @@ def main():
 
     :return:
     """
-    mtd = load(f"data/{MULTI_TRAIN_FILE_NAME}")
-    mtd_trial = load(f"data/{MULTI_TRIAL_FILE_NAME}")
+    try:
+        if sys.argv[1] == "-S":
+            td = load(f"data/{SINGLE_TRAIN_FILE_NAME}")
+            td_trial = load(f"data/{SINGLE_TRIAL_FILE_NAME}")
+        elif sys.argv[1] == "-M":
+            td = load(f"data/{MULTI_TRAIN_FILE_NAME}")
+            td_trial = load(f"data/{MULTI_TRIAL_FILE_NAME}")
+    except IndexError:
+        exit("Please specify which type of trial data you would like"
+             " to use (-S for single trial, -M for multi trial data)!")
 
-    X_train, y_train = extract_features(mtd), mtd[['complexity']]
-    X_trial, y_trial = extract_features(mtd_trial), mtd_trial[['complexity']]
+    X_train, y_train = extract_features(td), td[['complexity']]
+    X_trial, y_trial = extract_features(td_trial),\
+                       td_trial[['complexity']]
     X_train = X_train[['sentence_length', 'sentence_word_count',
                  'sentence_avg_word_length', 'sentence_vowel_count',
-                 'token_length', 'token_vowel_count']]
+                 'token_length', 'token_vowel_count', 'subcorpus']]
     X_trial = X_trial[['sentence_length', 'sentence_word_count',
                  'sentence_avg_word_length', 'sentence_vowel_count',
-                 'token_length', 'token_vowel_count']]
+                 'token_length', 'token_vowel_count', 'subcorpus']]
 
     pipeline = Pipeline([
         # ('scaler', StandardScaler()),
@@ -99,6 +108,7 @@ def extract_features(dataframe, use_token=True):
             "sentence_word_count"]).astype(int)
     dataframe["sentence_vowel_count"] = dataframe[
         "sentence"].str.lower().str.count(r'[aeiou]')
+    dataframe["subcorpus"] = ENCODER.fit_transform(dataframe["subcorpus"])
 
     if use_token:
         dataframe["token_length"] = dataframe["token"].str.len()
