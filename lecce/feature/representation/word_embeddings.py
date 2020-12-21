912bf7a75dbc88413e61ef1be3a82c3ef8a4905d
Leon Wetzel <leonwetzel@gmail.com>
2020-12-21 14:45:32 +0100

    Updated word_embeddings.py with functionality to transform text by using Fasttext

--- lecce/feature/representation/word_embeddings.py
+++ lecce/feature/representation/word_embeddings.py
@@ -59,14 +59,15 @@ class Word2VecEmbedder(Embedder):
         elif corpus:
             model_name = input("Please enter the filename for your new "
                                "word2vec model (including extension): ")
-            self.model = Word2Vec(sentences=corpus, size=300,
-                                  alpha=0.025, window=5, min_count=5,
-                                  max_vocab_size=None, sample=0.001, seed=1,
-                                  workers=3, min_alpha=0.0001, sg=0, hs=0,
-                                  negative=5, ns_exponent=0.75, cbow_mean=1,
-                                  iter=5, null_word=0, trim_rule=None,
-                                  sorted_vocab=1, batch_words=10000,
-                                  compute_loss=False, callbacks=(),
+            self.model = Word2Vec(sentences=corpus, corpus_file=None,
+                                  vector_size=300, alpha=0.025, window=5,
+                                  min_count=1, max_vocab_size=None,
+                                  sample=0.001, seed=1, workers=3,
+                                  min_alpha=0.0001, sg=0, hs=0, negative=5,
+                                  ns_exponent=0.75, cbow_mean=1, epochs=5,
+                                  null_word=0, trim_rule=None, sorted_vocab=1,
+                                  batch_words=10000, compute_loss=False,
+                                  callbacks=(), comment=None,
                                   max_final_vocab=None)
             self.model.save(f"{directory}/{model_name}")
         else:
@@ -79,9 +80,46 @@ class Word2VecEmbedder(Embedder):
 
 
 class FastTextEmbedder(Embedder):
+    """
+    This class contains functions for the use of word2vec embeddings
+    with LECCE functionality.
+    """
     def __init__(self, model_name="my_first_ft_model.bin",
                  corpus=None, directory="embeddings"):
+        """
+
+        Parameters
+        ----------
+        model_name : str
+            File name of the model.
+        corpus : list
+            Collection of sentences.
+        directory : str
+            Name of the directory containing the word embeddings.
+
+        """
         super(FastTextEmbedder, self).__init__(model_name, corpus, directory)
+
+        if model_name and not corpus:
+            self.model = FastText.load(f"{directory}/{model_name}")
+        elif corpus:
+            model_name = input("Please enter the filename for your new "
+                               "fasttext model (including extension): ")
+            self.model = FastText(sentences=corpus, corpus_file=None, sg=0, hs=0,
+                                  vector_size=300, alpha=0.025, window=5,
+                                  min_count=5, max_vocab_size=None,
+                                  word_ngrams=1, sample=0.001, seed=1,
+                                  workers=3, min_alpha=0.0001, negative=5,
+                                  ns_exponent=0.75, cbow_mean=1, epochs=5,
+                                  null_word=0, min_n=3, max_n=6, sorted_vocab=1,
+                                  bucket=2000000, trim_rule=None,
+                                  batch_words=10000, callbacks=(),
+                                  max_final_vocab=None)
+            self.model.save(f"{directory}/{model_name}")
+        else:
+            download_google_news_vectors(
+                destination_name=f"{directory}/GoogleNews-vectors.bin"
+            )
 
     def transform(self):
         pass
